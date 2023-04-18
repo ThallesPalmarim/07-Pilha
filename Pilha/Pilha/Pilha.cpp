83bc91687f12d8e38c78b4e608eb10f0bcacdb19
ThallesPalmarim <125374007+ThallesPalmarim@users.noreply.github.com>
2023-04-18 10:42:34 -0300

    Update Pilha.cpp

--- Pilha/Pilha/Pilha.cpp
+++ Pilha/Pilha/Pilha.cpp
@@ -14,6 +14,7 @@ void menu();
 void inicializar();
 void pop();
 void push();
+void exibirElementos();
 //--------------------------
 
 
@@ -25,14 +26,15 @@ int main()
 void menu()
 {
 	int op = 0;
-	while (op != 4) {
+	while (op != 5) {
 		system("cls"); // somente no windows
 		cout << "Menu Pilha";
 		cout << endl << endl;
 		cout << "1 - Inicializar Pilha \n";
 		cout << "2 - Inserir elemento (Push) \n";
 		cout << "3 - Remover elementos (Pop) \n";
-		cout << "4 - Sair \n";
+		cout << "4 - Exibir elementos \n";
+		cout << "5 - Sair \n";
 
 
 		cout << "Opcao: ";
@@ -46,7 +48,9 @@ void menu()
 			break;
 		case 3: pop();
 			break;
-		case 4:
+		case 4: exibirElementos();
+			break;
+		case 5:
 			return;
 		default:
 			break;
@@ -59,7 +63,7 @@ void menu()
 void inicializar()
 {
 
-	// se a lista já possuir elementos
+	// se a lista jÃ¡ possuir elementos
 	// libera a memoria ocupada
 	NO* aux = topo;
 	while (aux != NULL) {
@@ -73,6 +77,21 @@ void inicializar()
 
 }
 
+void exibirElementos()
+{
+	if (topo == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = topo;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->prox;
+		}
+	}
+}
 
 void push()
 {
@@ -86,14 +105,28 @@ void push()
 	cout << "Digite o elemento: ";
 	cin >> novo->valor;
 	novo->prox = NULL;
-
-
+	if (topo == NULL)
+	{
+		topo = novo;
+	}
+	else
+	{
+		novo->prox = topo;
+		topo = novo;
+	}
 }
 
 void pop()
 {
-
-	
+	if (topo == NULL)
+	{
+		cout << "Pilha vazia\n";
+	}
+	else
+	{
+		NO* aux = topo;
+		topo = aux->prox;
+		free(aux);
+	}
 
 }
-
